155ec64322e4c8286be7db520b77e3d398e4ddf3
Nishandhini-S <717823p236@kce.ac.in>
2026-02-02 09:02:57 +0530

    Initial commit

---
+++ src/com/repair/util/TechnicianNotSuitableException.java
@@ -0,0 +1,13 @@
+package com.repair.util;
+
+public class TechnicianNotSuitableException extends Exception {
+    private String message;
+    public TechnicianNotSuitableException(String message) {
+        super(message);   // keep message in the base Exception as well
+        this.message = message;
+    }
+    @Override
+    public String toString() {
+        return "Technician Not Suitable: " + message;
+    }
+}
